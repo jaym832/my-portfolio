3c67afec3ab6014cd9e7268e3b05b9fa1617af87
Javier Marin <jjmarin777@gmail.com>
2021-07-21 16:06:12 -0500

    added test

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,6 +8,7 @@ function About() {
             <div className='about-text'>
 
                 <h1>About me :</h1>
+                test
                 My name is Javier Marin and i am a junior full stack developer. I am currently
                 a senior at the University of Houston where i am majoring in Computyer Information
                 Systems. I am also a graduate in the Software engineering program at Flatiron school.
