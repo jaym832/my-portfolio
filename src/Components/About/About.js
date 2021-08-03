dad284e3ecfd7527c893f490661e889638632f0b
Javier Marin <jjmarin777@gmail.com>
2021-08-02 23:08:33 -0500

    another one

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -16,7 +16,7 @@ function About() {
                     Programming is a bittersweet art form for me. One minute i could be loving it and then the next i could be
                     wanting to tear my hair out trying to debug an error, but thats what i enjoy about it, the challenge and the gratification
                     i get from it. Programming is a job/hobby that is always changing and you have to be ok with never really
-                    knowing everything about it,its something you are constantly learning. Thats a journey im willing to take.
+                    knowing everything about it.Its something where you have to be ok with always learning and adapating,and thats a journey im willing to take.
                     <br></br><br></br>
                     When im not typing away at my keyboard you can find me hanging out with family,watching a movie, working out, or reading.
                 </p>
