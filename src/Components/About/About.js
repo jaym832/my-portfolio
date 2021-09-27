d94c1b638fc0a566122d613ccd685847473c59ae
Javier Marin <jjmarin777@gmail.com>
2021-09-27 17:00:00 -0500

    more info

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -11,13 +11,18 @@ function About() {
 
                 <p> My name is Javier Marin and I am a junior full stack developer and currently
                     a senior at the University of Houston where I am majoring in Computer Information
-                    Systems. I am also a graduate in the Software engineering program at Flatiron school.
+                    Systems. I am also a graduate in the Software engineering program at Flatiron.
                     <br></br><br></br>
                     Programming is a bittersweet art form for me. One minute I could be loving it and then the next I could be
-                    wanting to tear my hair out trying to debug an error, but thats what I enjoy about it, the challenge and the gratification
-                    I get from it. Programming is a job/hobby that is always changing.Its something where you have to be ok with always learning and adapating,and thats a journey im willing to take.
+                    wanting to tear my hair out trying to debug an error, but thats what I really enjoy about it, the challenge and the gratification
+                    I get from it. Programming is a job/hobby that is always constantly changing.Its something where you have to be ok with always learning and adapating.
+
+
+
                     <br></br><br></br>
-                    When im not typing away at my keyboard you can find me hanging out with family,watching a movie, working out, or reading.
+                    When im not typing away at my keyboard you can find me hanging out with my family,watching a movie, working out, or reading a book.
+                    I am always looking for projects that can help me grow and learn new technologies. Feel free to send me a message if you are interested
+                    in collaborating with me or just want to talk.
                 </p>
             </div>
 
